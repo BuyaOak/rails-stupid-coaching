af88ee5ddb9d3be638fb7f90168bb2aa8283537a
ayub bey <ayub.bey@gmail.com>
2021-05-22 15:24:36 +0800

    finally

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+
+
+def home
+end
+
+def ask
+end
+
+def answer
+  if params[:question] == "I am going to work"
+    @answer = "Great"
+  else
+    @answer = "I don't care, get dressed and go to work!" 
+  end
+  end
+end     
